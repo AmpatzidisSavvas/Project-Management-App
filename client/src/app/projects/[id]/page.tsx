800a3058baee32452c40a5b451fb7cfa02093a6e
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-10-01 12:16:19 +0300

    BoardView complete

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react'
 import ProjectHeader from "../ProjectHeader";
+import Board from '../BoardView';
 
 type Props = {
     params: { id: string }
@@ -17,9 +18,9 @@ const Project = ({ params }: Props) => {
     <div>
         {/* MODAL NEW TASKS */}
         <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-        {/* { activeTab === "Board" && (
-            <Board />
-        )} */}
+        { activeTab === "Board" && (
+            <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
+        )}
     </div>
   )
 }
