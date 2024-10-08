668bfa3d372e637ad61ad70f635b7c08157d74ad
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-10-08 10:57:57 +0300

    Teams page complete

---
+++ server/src/routes/teamRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { getUsers } from "../controllers/userController";
+import { getTeams } from "../controllers/teamController";
+
+const router = Router();
+
+router.get("/", getTeams);
+
+
+export default router;
